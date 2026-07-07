41371448b3bfd260b8aa9428364c541d65d2060b
Vipin Jaitly <vipinjaitly92@gmail.com>
2026-07-07 19:29:32 +0530

    Added user info Python practice

---
+++ userinfo.py
@@ -0,0 +1,13 @@
+name = "Vipin Jetely"
+company = "Brothers Secure Pvt. Ltd."
+age = 44
+experience = 20
+salary = 250000
+is_business_owner = True
+
+print ("Name :", name)
+print ("Company :", company)
+print ("Age :", age)
+print ("Experience :", experience, "years")
+print ("Salary :", salary)
+print ("Is Business Owner :", is_business_owner)
