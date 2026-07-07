62aec7bd4a2b7e590cf17e18b542c16343de2c47
Vipin Jaitly <vipinjaitly92@gmail.com>
2026-07-07 18:05:33 +0530

    Learned Python data types

---
+++ datatypes.py
@@ -0,0 +1,13 @@
+company = "Brothers Secure Pvt. Ltd."
+director = "Vipin Jetely"
+quantity = 10
+price = 2500
+gst = 18.0
+is_paid = True
+
+print(type(company))
+print(type(director))
+print(type(quantity))
+print(type(price))
+print(type(gst))
+print(type(is_paid))
---
+++ variables.py
@@ -0,0 +1,15 @@
+company = "Brothers Secure Pvt. Ltd."
+director = "Vipin Jetely"
+client = "Mobi Ocean"
+
+quantity = 10
+price = 2500
+
+total = quantity * price
+
+print(company)
+print(director)
+print(client)
+print(quantity)
+print(price)
+print(total)
