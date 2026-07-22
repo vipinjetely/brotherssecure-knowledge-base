27c0f8100b9d4e6d152dc7a8a4691c111eda63de
Vipin Jaitly <vipinjaitly92@gmail.com>
2026-07-23 01:20:02 +0530

    Update nested_dictionary.py with nested dictionary examples

---
+++ nested_dictionary.py
@@ -0,0 +1,10 @@
+company = {
+    "Client": {
+        "Name": "Mobi Ocean",
+        "City": "Delhi"
+    }
+}
+
+print(company["Client"])
+print(company["Client"]["Name"])
+print(company["Client"]["City"])
